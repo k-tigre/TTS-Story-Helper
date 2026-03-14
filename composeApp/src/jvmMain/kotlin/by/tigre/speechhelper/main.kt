2b8f2b2be4430c460c49a732c1e23be77fd4b2ba
ktigre <ktigre@yandex-team.ru>
2026-03-14 17:20:46 +0300

    First version

---
+++ composeApp/src/jvmMain/kotlin/by/tigre/speechhelper/main.kt
@@ -0,0 +1,16 @@
+package by.tigre.speechhelper
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "SpeechHelper",
+        state = rememberWindowState(width = 800.dp, height = 600.dp),
+    ) {
+        App()
+    }
+}
