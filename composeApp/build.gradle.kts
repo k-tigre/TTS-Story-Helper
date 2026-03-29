597208a8d69f35573ccabd9e112d73d05769180d
Tigre <k.gorbats@gmail.com>
2026-03-29 13:48:12 +0300

    Updated app version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -47,7 +47,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "by.tigre.speechhelper"
-            packageVersion = "1.0.0"
+            packageVersion = "1.1.0"
 
             macOS {
                 signing {
